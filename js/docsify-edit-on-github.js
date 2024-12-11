494a0cd85a846f4a84c26d2bd1c3bfbc3e322201
孔余 <2385569970@qq.com>
2024-12-11 19:06:50 +0800

    修改文档

--- js/docsify-edit-on-github.js
+++ js/docsify-edit-on-github.js
@@ -1,9 +1,62 @@
-/**
- * Minified by jsDelivr using Terser v5.19.2.
- * Original file: /npm/docsify-edit-on-github@1.0.3/index.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-!function(t){t.EditOnGithubPlugin={},t.EditOnGithubPlugin.create=function(n,i,e){function u(t){return header=['<div style="overflow: auto">','<p style="float: right"><a style="text-decoration: underline; cursor: pointer"','onclick="EditOnGithubPlugin.onClick(event)">',t,"</a></p>","</div>"].join("")}return e=e||"Edit on github",i=i||n.replace(/\/blob\//,"/edit/"),t.EditOnGithubPlugin.editDoc=function(t,n){var e=n.route.file;if(e){var u=i+e;return window.open(u),t.preventDefault(),!1}return!0},function(n,i){if(t.EditOnGithubPlugin.onClick=function(t){EditOnGithubPlugin.editDoc(t,i)},(r=e)&&"[object Function]"==={}.toString.call(r))n.afterEach((function(t){return u(e(i.route.file))+t}));else{var o=u(e);n.afterEach((function(t){return o+t}))}var r}}}(window);
-//# sourceMappingURL=/sm/54cff3a4c90a14c5530ed6edd48405c4df7d5a42b7dcf9ed73a0fe89ed4d0105.map
+;(function(win) {
+  function isFunction(functionToCheck) {
+   return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]'
+  }
+
+  win.EditOnGithubPlugin = {}
+
+  function create(docBase, docEditBase, title) {
+    title = title || 'Edit on github'
+    docEditBase = docEditBase || docBase.replace(/\/blob\//, '/edit/')
+
+    function editDoc(event, vm) {
+      var docName = vm.route.file
+
+      if (docName) {
+        var editLink = docEditBase + docName
+        window.open(editLink)
+        event.preventDefault()
+        return false
+      } else {
+        return true
+      }
+    }
+
+    win.EditOnGithubPlugin.editDoc = editDoc
+
+    function generateHeader(title) {
+      return header = [
+        '<div style="overflow: auto">',
+        '<p style="float: right"><a style="text-decoration: underline; cursor: pointer"',
+        'onclick="EditOnGithubPlugin.onClick(event)">',
+        title,
+        '</a></p>',
+        '</div>'
+      ].join('')
+    }
+
+    return function(hook, vm) {
+      win.EditOnGithubPlugin.onClick = function(event) {
+        EditOnGithubPlugin.editDoc(event, vm)
+      }
+
+      if (isFunction(title)) {
+
+        hook.afterEach(function (html) {
+          return generateHeader(title(vm.route.file)) + html
+        })
+      } else {
+        var header = generateHeader(title)
+
+        hook.afterEach(function (html) {
+          return header + html
+        })
+      }
+
+
+    }
+  }
+
+  win.EditOnGithubPlugin.create = create
+}) (window)
 
